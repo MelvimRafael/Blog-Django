932a22d4963d74e0f2d71396f4f4027cd88d9ad7
MelvimRafael <melvimrafaelteclas@gmail.com>
2022-10-18 23:32:22 -0300

    comentarios"

--- blog/settings.py
+++ blog/settings.py
@@ -136,3 +136,13 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING:'alert-warning',
+    constants.DEBUG:'alert-info',
+    constants.SUCCESS:'alert-success',
+    constants.INFO:'alert-info',
+}
--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     autor_post = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name= 'Autor')
     data_post = models.DateTimeField(default=timezone.now, verbose_name= 'Data')
     conteudo_post = models.TextField(verbose_name= 'Conteúdo')
-    excerto_post = models.CharField(max_length=500)
+    excerto_post = models.TextField()
     categoria_post = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, blank=True, null=True, verbose_name= 'Categoria')
     imagem_post = models.ImageField(upload_to='post_img/%Y/%m/%d', blank=True , null= True )
     publicado_post = models.BooleanField(default=False, verbose_name= 'Publicado')
